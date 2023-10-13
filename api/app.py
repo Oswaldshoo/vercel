b59afd4b08fab183e846feecb50044a3e37b5a6a
oswald shoo <oswaldshoo6@gmail.com>
2023-10-13 15:27:36 +0300

    error fix

--- api/app.py
+++ api/app.py
@@ -3,12 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def main():
-    register()
-    about()
-    login()
-    contact()
-    app.run(debug=True)
 
 @app.route("/")
 def hello_world():
@@ -30,6 +24,4 @@ def login():
 def register():
     return render_template('register.html')
 
-if __name__ == "__main__":
-    main()
 
